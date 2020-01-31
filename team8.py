32967a9df90804420a83ba5ad446b08c486a85a6
BrianDinh91 <jkljkla85@gmail.com>
2020-01-31 11:02:16 -0800

    Move method and test_move method update

--- team8.py
+++ team8.py
@@ -13,17 +13,71 @@ team_name = 'collusion8' # Only 10 chars displayed
 strategy_name = 'Collude'
 strategy_description = 'Always collude.'
     
+####
+# Each team's file must define four tokens:
+#     team_name: a string
+#     strategy_name: a string
+#     strategy_description: a string
+#     move: A function that returns 'c' or 'b'
+####
+
+#version 1/28/30
+team_name = 'collusion8' # Only 10 chars displayed
+strategy_name = 'Collude'
+strategy_description = 'Always collude.'
+    
+justbetrayed = False #boolean for one of the strategy to indicate that we've just betrayed.    
 def move(my_history, their_history, my_score, their_score):
-    '''Make my move based on the history with this player.
+    ''' Arguments accepted: my_history, their_history are strings.
+    my_score, their_score are ints.
     
-    history: a string with one letter (c or b) per round that has been played with this opponent.
-    their_history: a string of the same length as history, possibly empty. 
-    The first round between these two players is my_history[0] and their_history[0]
-    The most recent round is my_history[-1] and their_history[-1]
-    
-    Returns 'c' or 'b' for collude or betray.
+    Make my move.
+    Returns 'c' or 'b'. 
     '''
+
+    # my_history: a string with one letter (c or b) per round that has been played with this opponent.
+    # their_history: a string of the same length as history, possibly empty. 
+    # The first round between these two players is my_history[0] and their_history[0].
+    # The most recent round is my_history[-1] and their_history[-1].
     
-    # This player always colludes.
-    return 'c'
+    # Analyze my_history and their_history and/or my_score and their_score.
+    # Decide whether to return 'c' or 'b'.
     
+    #return 'c'
+    
+    
+    if(len(my_history) < 3):
+        return 'c'
+    else:
+        return 'b'
+    #Other possible strategies
+    #if(len(my_history) == 1):
+    #   return 'c'
+    #else:
+    #   return 'b'
+    
+    
+    #if(len(my_history) == 0):
+    #   return 'c'
+    #else:
+    #   return 'b'
+    
+    #if(justbetrayed):
+    #   justbetrayed = False
+    #   return 'b'
+    #   
+    #elif((my_score - their_score) <= 0):
+    #   justbetrayed = True
+    #   return 'b'
+    #elif(my_score - their_score >= 0):
+    #   return 'c'
+    
+    
+def test_move(my_history, their_history, my_score, their_score, result):
+    '''calls move(my_history, their_history, my_score, their_score)
+    from this module. Prints error if return value != result.
+    Returns True or False, dpending on whether result was as expected.
+    '''
+    real_result = move(my_history, their_history, my_score, their_score)
+    if real_result == result:
+        return True
