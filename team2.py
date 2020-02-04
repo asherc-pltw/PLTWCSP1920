e5ff7aafcc09190839c4a0b2e614e9d6ce1e6138
UdbhavAvadhani <60272107+UdbhavAvadhani@users.noreply.github.com>
2020-02-04 11:10:18 -0800

    Update team2.py

--- team2.py
+++ team2.py
@@ -6,23 +6,28 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'Pentagofu' # Only 10 chars displayed
-strategy_name = 'Alternate'
-strategy_description = 'Collude, then alternate.'
+team_name = 'Pentagofu'
+strategy_name = 'Betray, Opposite, and History'
+strategy_description = 'Betray first, then do opposite of opponent for 3 moves, then go based off total history.'
     
 def move(my_history, their_history, my_score, their_score):
-    '''Make my move based on the history with this player.
-    
-    history: a string with one letter (c or b) per round that has been played with this opponent.
-    their_history: a string of the same length as history, possibly empty. 
-    The first round between these two players is my_history[0] and their_history[0]
-    The most recent round is my_history[-1] and their_history[-1]
-    
-    Returns 'c' or 'b' for collude or betray.
-    '''
-    # This player colludes on even numbered rounds (first round is round #0).
-    if len(my_history)%2 == 0:
+    b_counter = 0
+    c_counter = 0
+    nomoves = b_counter + c_counter
+    if their_history == '':
+       return 'b'
+    if their_history[-1] == 'b':
+        b_counter += 1
+    elif their_history[-1] == 'c':
+        c_counter += 1
+    if their_history[nomoves] == 'b' and nomoves < 5:
         return 'c'
-    else:
+    elif their_history[nomoves] == 'c' and nomoves < 5:
         return 'b'
-    
+    elif nomoves >= 5:
+        if c_counter > b_counter:
+            return 'b'
+        elif c_counter < b_counter:
+            return 'c'
+        else:
+            return 'c'
