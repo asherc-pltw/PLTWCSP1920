c63f909138a4aa76a6fbd5d196f4e53764636792
545812 <60403448+545812@users.noreply.github.com>
2020-02-04 10:56:52 -0800

    Update team6.py

--- team6.py
+++ team6.py
@@ -7,22 +7,27 @@
 ####
 
 team_name = 'team6' # Only 10 chars displayed
-strategy_name = 'Alternate'
-strategy_description = 'Collude, then alternate.'
+strategy_name = 'whatStrategy'
+strategy_description = 'if our score drops below a certain range, it plays more aggressively.'
     
 def move(my_history, their_history, my_score, their_score):
-    '''Make my move based on the history with this player.
-    
-    history: a string with one letter (c or b) per round that has been played with this opponent.
-    their_history: a string of the same length as history, possibly empty. 
-    The first round between these two players is my_history[0] and their_history[0]
-    The most recent round is my_history[-1] and their_history[-1]
-    
-    Returns 'c' or 'b' for collude or betray.
+    '''So we gonna have a big overall conditional?? maybe. if (our points? (poss??)
+    are above a certain range, like >-2000, be less agressive, by using more 'c'. 
+    If <= -2000, then go more aggressive, like more 'b' used instead of 'c'. There
+    would be conditionals inside this broad one as well tho... idk. So to have a 
+    'less' agressive strats would be like... 
     '''
-    # This player colludes on even numbered rounds (first round is round #0).
-    if len(my_history)%2 == 0:
-        return 'c'
-    else:
+    while my_score==0:
+        return 'c'      
+    if my_score>-2000:
+        if my_history[-1]== 'c' and their_history[-1]=='c':
+            return 'c'
+        elif my_history[-1]=='c' and their_history[-1]=='b':
+            return 'b'
+        else:
+            return 'b'
+    elif my_score<=-2000:
+        return'b'
+        
+    else: 
         return 'b'
-    
